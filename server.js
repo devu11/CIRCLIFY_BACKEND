39de84efff9597042f03de8e9d3a43f89d009fa7
devu11 <devudevzz3@gmail.com>
2024-03-29 17:38:06 +0530

    socket

--- server.js
+++ server.js
@@ -1,55 +1,96 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import { configDotenv } from "dotenv";
+import { config } from "dotenv";
+import cors from 'cors';
+import http from 'http';
+import { Server } from "socket.io";
+
+// Import your routes here
 import AuthRoute from './Routes/AuthRoute.js';
- import UserRoute from './Routes/UserRoute.js';
- import UploadRoute from './Routes/UploadRoute.js';
- import AdminRoute from './Routes/AdminRoute.js';
- import PostRoute from './Routes/PostRoute.js'
- import ChatRoute from './Routes/ChatRoute.js'
- import MessageRoute from './Routes/MessageRoute.js'
- import cors from 'cors'
-const app = express()
+import UserRoute from './Routes/UserRoute.js';
+import UploadRoute from './Routes/UploadRoute.js';
+import AdminRoute from './Routes/AdminRoute.js';
+import PostRoute from './Routes/PostRoute.js';
+import ChatRoute from './Routes/ChatRoute.js';
+import MessageRoute from './Routes/MessageRoute.js';
 
-
-
-app.use(express.static('public'))
-app.use('/images', express.static("images"))
-
-
-
-
- 
-app.use(bodyParser.json({ limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors())
-
-app.use((req, res, next) => {
-  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-  res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-  next();
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+ cors: {
+    origin: ["http://localhost:3000", "https://circlify-theta.vercel.app/"],
+ },
 });
 
+app.use(express.static('public'));
+app.use('/images', express.static("images"));
 
-configDotenv();
-mongoose
-  .connect(process.env.MONGO_DB)
-  .then(() => {
-    console.log("MongoDB Connected Successfulyy!!!");
-    app.listen(process.env.PORT, () => console.log("Server Started"));
-  })
-  .catch((error) => console.error("MongoDB connection error:", error));
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
+app.use((req, res, next) => {
+ res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+ res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+ next();
+});
 
-//    routes
+config();
+mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true })
+ .then(() => {
+    console.log("MongoDB Connected Successfully!!!");
+    server.listen(process.env.PORT, () => console.log("Server Started"));
+ })
+ .catch((error) => console.error("MongoDB connection error:", error));
 
-app.use('/auth', AuthRoute)
-app.use('/user', UserRoute)
-app.use('/upload',UploadRoute)
-app.use('/post',PostRoute)
-app.use('/',AdminRoute)
-app.use('/chat', ChatRoute)
-app.use('/message', MessageRoute)
+// Routes
+app.use('/auth', AuthRoute);
+app.use('/user', UserRoute);
+app.use('/upload', UploadRoute);
+app.use('/post', PostRoute);
+app.use('/', AdminRoute);
+app.use('/chat', ChatRoute);
+app.use('/message', MessageRoute);
 
+let activeUsers = [];
 
+io.on("connection", (socket) => {
+ console.log("A user connected");
+
+ socket.on("new-user-add", (newUserId) => {
+    if (!activeUsers.some((user) => user.userId === newUserId)) {
+      activeUsers.push({
+        userId: newUserId,
+        socketId: socket.id,
+      });
+    }
+    console.log("Connected Users", activeUsers);
+    io.emit("get-users", activeUsers);
+ });
+
+ socket.on("send-message", (data) => {
+    const { receiverId, senderId, text, senderUsername } = data;
+    console.log("Sending from Socket to:", receiverId);
+    console.log("Data:", data);
+    if (receiverId) {
+      const user = activeUsers.find((user) => user.userId === receiverId);
+      if (user) {
+        io.to(user.socketId).emit("receive-message", data);
+        io.to(user.socketId).emit("receive-notification", {
+          message: `You have a new message from ${senderUsername}: ${text}`,
+        });
+      } else {
+        console.log("User not found");
+      }
+    } else {
+      console.log("Receiver ID is null");
+    }
+ });
+
+ socket.on("disconnect", () => {
+    activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
+    console.log("User Disconnected", activeUsers);
+    io.emit("get-users", activeUsers);
+ });
+});
